fe896d35c2f0e9a49293ffcad41c951f2e952f12
JJBIoxxer <71458383+JJBIoxxer@users.noreply.github.com>
2021-03-31 14:21:34 -0700

    Add files via upload

--- src/index.lua
+++ src/index.lua
@@ -1,1 +1,14 @@
+local Rohook = {}
+Rohook.__index = Rohook
 
+--> Require all the utility functions needed for this module.
+local _Unpack = require(script.Parent.util.Unpack)
+
+--> Load all the constructors using the "Unpack" utility function.
+_Unpack(script.Parent.constructors, Rohook)
+
+--> Create a watermark in the console.
+print("Rohook (V0.2.1.BETA) by JJBIoxxer has loaded successfully.")
+
+--> Return the module data.
+return Rohook
