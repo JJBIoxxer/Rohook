4d1c56327e11fd81fc7e8e21ea74bae7ba3bd23f
JJBIoxxer <71458383+JJBIoxxer@users.noreply.github.com>
2021-04-01 13:17:28 -0700

    Update Unpack.lua

--- src/util/Unpack.lua
+++ src/util/Unpack.lua
@@ -1,12 +1,12 @@
-function readdir(parent, table)
+function _unpack(parent, table)
 	for _, child in next, parent:GetChildren() do
 		if (child.ClassName == "ModuleScript") then
 			table[child.Name] = require(child)
 		elseif (child.ClassName == "Folder") then
 			table[child.Name] = {}
-			readdir(child, table[child.Name])
+			_unpack(child, table[child.Name])
 		end
 	end
 end
 
-return readdir
+return _unpack
